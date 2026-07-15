6ee8af1b0a9e53233ad3eed7f3ab20e286763765
valrey <vikei.ar03@gmail.com>
2026-07-15 17:44:58 -0600

    cambios en el archivo main

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ for i in range(1,1001):
     else:
         print(i)
         
-# i=0
-# while i < 21:
-#     if i%3==0 and i%5==0:
-#         print("Fizzbuzz")
-#     elif i%3==0:
-#         print("Fizz")
-#     elif i%5==0:
-#         print("Buzz")
-#     else:
-#         print(i)
-#     i += 1
+i=0
+while i < 21:
+    if i%3==0 and i%5==0:
+        print("Fizzbuzz")
+    elif i%3==0:
+        print("Fizz")
+    elif i%5==0:
+        print("Buzz")
+    else:
+        print(i)
+    i += 1
